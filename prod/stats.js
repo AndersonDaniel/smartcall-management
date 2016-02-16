6d39a931daf44cadde558f7a9693078832bc6cc1
daniel anderson <daniel@daniel-pc.(none)>
2016-02-16 21:20:02 +0200

    Viewing logs in table & copying to clipboard

--- prod/stats.js
+++ prod/stats.js
@@ -3,13 +3,30 @@ smartApp = angular.module('smartcall', []);
 smartApp.controller('statsController', ['$scope', '$location', function($scope, $location) {
 	var self = this;
 	
-	var myDataRef = new Firebase("https://smartcall-management.firebaseio.com/clients");
+	var myDataRef = new Firebase("https://smartcall-management.firebaseio.com/logs");
 	
-	alert($location.search()['app']);
+	self.displayID = $location.search()['app'];
+	
+	self.copy = function() {
+		var textArea = document.createElement('textarea');
+		var NEW_LINE = String.fromCharCode(10);
+		textArea.value = ['אפליקציה', 'מחלקה', 'מספר משתמש', 'מועד בקשה'].join('\t') + NEW_LINE;
+		textArea.value += 
+			self.logs.map(function(log) { return (['appName', 'tenantName', 'phone', 'when'].map(function(k) { return log[k]; }).join('\t')); }).join(NEW_LINE);
+		document.body.appendChild(textArea);
+		textArea.focus();
+		textArea.select();
+		document.execCommand('copy');
+		document.body.removeChild(textArea);
+	};
 	
 	myDataRef.on('value', function(snapshot){
 			if (snapshot.val()){
-				
+				$scope.$apply(function(){
+					self.logs = snapshot.val().filter(function(row) {
+						return (row.appID == self.displayID);
+					});	
+				});
 			}
 	});
 }]);
